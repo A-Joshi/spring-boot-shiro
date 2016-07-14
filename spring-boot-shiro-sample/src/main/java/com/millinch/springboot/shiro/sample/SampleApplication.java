36835e92b3d77a5af9144659597eded27a564133
amit.joshi@markit.com <amit.joshi@markit.com>
2016-07-13 22:21:26 -0400

    Change to spring boot 1.4.0.RC1

--- spring-boot-shiro-sample/src/main/java/com/millinch/springboot/shiro/sample/SampleApplication.java
+++ spring-boot-shiro-sample/src/main/java/com/millinch/springboot/shiro/sample/SampleApplication.java
@@ -3,11 +3,23 @@ package com.millinch.springboot.shiro.sample;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+
 /**
  * This guy is lazy, nothing left.
  *
  * @author John Zhang
  */
+@Configuration
+@EnableSwagger2
+@ComponentScan
+@EnableAutoConfiguration
 @SpringBootApplication
 public class SampleApplication {
 
